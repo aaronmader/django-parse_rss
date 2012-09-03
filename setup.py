de8fed83cf16c5f1c97d7fb9b2df3c1190db1726
Frank <frank@hub.nl>
2012-09-03 10:42:47 +0300

    Removing packages

--- setup.py
+++ setup.py
@@ -5,15 +5,11 @@ long_description = open('README.md').read()
  
 setup(
   name='django-parse_rss',
-  version='1.0',
+  version='1.0.0',
   description='Django rss reader',
   long_description=long_description,
   author='Frank van der Pluijm',
   author_email='f.vd.pluijm@hub.nl',
   url='https://github.com/frankvdp/django-parse_rss',
-  packages=(
-    'flag',
-    'flag.templatetags',
-  ),
   zip_safe=False,
 )
